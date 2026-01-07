7e72d14edd3e89f9b666f93c80bf09232a3846d1
XIMENES Carlos Ximenes (parceiro) <CXXIMENES@parceiro.mc.pt>
2026-01-07 13:55:22 -0300

    fix issues

--- src/app/pages/result/result.ts
+++ src/app/pages/result/result.ts
@@ -1,4 +1,4 @@
-import { Component, Signal } from '@angular/core';
+import { Component, ElementRef, Signal, ViewChild } from '@angular/core';
 import { List } from './list/list';
 import { TournamentStore } from '../../store/tournament.store';
 import { Router } from '@angular/router';
@@ -10,12 +10,18 @@ import { Router } from '@angular/router';
   styleUrl: './result.scss',
 })
 export class Result {
+  @ViewChild('scrollable') scrollable!: ElementRef;
+
   tournament!: Signal<any>;
   constructor(private _store: TournamentStore, private _router: Router) {
     this.tournament = this._store.top8Data; // this is a SIGNAL
 
-    if (!this.tournament || !this.tournament().id) {
+    if (!this.tournament || !this.tournament()?.id) {
       this._router.navigate(['/']);
     }
+  }
+
+  ngAfterViewInit(): void {
+    this.scrollable.nativeElement.scrollIntoView({ behavior: 'auto', block: 'start' });
   }
 }
